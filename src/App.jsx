43dc1da0cabbed3241fa45f0384e150a0cd885aa
Rachele Barberis <rachele.barberis.12@gmail.com>
2025-01-08 10:08:01 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return ( //<> è un parent element necessario
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
